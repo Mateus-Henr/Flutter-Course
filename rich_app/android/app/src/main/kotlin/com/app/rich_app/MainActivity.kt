2930107137ca4c84c8f8e3b69c83aec6bd3c7777
Matt <mateush.study@gmail.com>
2023-10-05 14:36:40 -0300

    first commit

---
+++ rich_app/android/app/src/main/kotlin/com/app/rich_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app.rich_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ rich_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // Flutter team advises to put a comma after every closing parethesis.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Main Page",
+      home: Center(
+        child: Text("Hello World"),
+      ),
+    );
+  }
+}
